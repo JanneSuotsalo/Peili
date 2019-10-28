85b4ef56395accb34646331402e281adf207c24a
JanneSuotsalo <janne.suotsalo@gmail.com>
2019-10-28 11:23:33 +0200

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Organization from './pages/organizations/Organization';
-import Profile from './pages/profile/Profile';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Organization from "./pages/organizations/Organization";
+import Profile from "./pages/profile/Profile";
 import { BrowserRouter, Switch, Route, Link, Router } from "react-router-dom";
 
-  
 function App() {
   return (
     <div>
-    <h1>NavBar</h1>
+      <BrowserRouter>
+        <div>
+          <ul id="nav">
+            <li>
+              <Link to="/profile">
+                <a>Profile</a>
+              </Link>
+            </li>
+            <li>
+              <Link to="/organization">
+                <a>Organization</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-    <BrowserRouter>
+        <Route exact path="/">
+          <Profile />
+        </Route>
         <Route path="/organization">
           <Organization />
         </Route>
         <Route path="/profile">
           <Profile />
         </Route>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
-
   );
 }
 
