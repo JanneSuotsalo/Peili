24fff975ba2726f53c3e29d9b59aa8932a249131
timpec <timpe96@hotmail.com>
2019-11-07 21:45:47 +0200

    feed added with side scrollers

--- src/components/cellList/CellList.js
+++ src/components/cellList/CellList.js
@@ -11,10 +11,15 @@ export default class CellList extends React.Component {
             <div className="listCard">
                 <div className="innerCard">
                     <img src={this.props.item.image} />
-                    <div>
-                        <h2>{this.props.item.name.fi}</h2>
-                        <h4>{this.props.item.datetype.type}</h4>
-                        <h4>{this.props.item.datetype.date}</h4>
+                    <div className="cardHeader">
+                        <h2>#{this.props.item.name.fi}</h2>
+                    </div>
+                    <div className="cardBody">
+                        <p>{this.props.item.description.body}</p>
+                    </div>
+                    <div className="cardFooter">
+                        <h4 className="footerItem">{this.props.item.datetype.type}</h4>
+                        <h4 className="footerItem">{this.props.item.datetype.date}</h4>
                     </div>
                 </div>
             </div>
