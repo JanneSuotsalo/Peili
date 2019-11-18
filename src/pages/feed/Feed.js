4b5abc06e45267a915a7ebc446ab869f771b9728
timpec <timpe96@hotmail.com>
2019-11-18 09:34:26 +0200

    Changes to feed page

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -3,14 +3,16 @@ import './Feed.css';
 import CellListView from '../../components/cellList/CellList';
 
 var data = require('../../example2.json');
+var taskData = require('../../taskListExample.json');
+var taskData2 = require('../../taskListExample2.json');
 
 export default class Feed extends React.Component {
     render() {
         return (
             <div>
-                <Cell />
-                <Cell />
-                <Cell />
+                <Cell title="Suositellut" />
+                <Tasks />
+                <Cell title="Kohtaus" />
             </div>
         )
     };
@@ -30,12 +32,77 @@ class Container extends React.Component {
 };
 
 class Cell extends React.Component {
+    constructor (props) {
+        super(props);
+        this.props = {title: "null"}
+    }
+
     render() {
         return (
             <div>
-                <h2 className="cellTitle">{data.name.fi}</h2>
+                <div className="cardHeader">
+                    <h2 className="sectionTitle">{this.props.title}</h2>
+                </div>
                 <Container />
             </div>
         )
     };
 };
+
+class Tasks extends React.Component {
+    constructor (props) {
+        super(props);
+        this.listTasks = this.listTasks.bind(this);
+    }
+
+    listTasks() {
+        var lTask = [];
+        var task = Element;
+        var task2 = Element;
+        //for (var i = 0; i < taskData.lenght; i++) {
+            //var obj = taskData[i];
+            //console.log(obj);
+
+            task = ( <div className="taskItem">
+                <div className="taskInner">
+                    <h2 className="taskText">{taskData.taskName}</h2>
+                    <h4 className="taskText"> Palkinto: {taskData.reward}</h4>
+                </div>
+            </div>)
+            lTask.push(task)
+            return lTask;
+
+            // Temporary examplefile #2
+
+
+            //lTask.push(task);
+            //}
+        //return lTask
+    }
+
+    listTasks2() {
+        var lTask = [];
+        var task2 = Element;
+
+            task2 = ( <div className="taskItem">
+                <div className="taskInner">
+                    <h2 className="taskText">{taskData2.taskName}</h2>
+                    <h4 className="taskText"> Palkinto: {taskData2.reward}</h4>
+                </div>
+            </div>)
+            lTask.push(task2)
+            return lTask;
+    }
+
+    render () {
+        return (
+            <div className="taskList">
+                <div className="taskHeader">
+                    <h2 className="sectionTitle">Kyselyt</h2>
+                </div>
+                {this.listTasks()}
+                {this.listTasks2()}
+            </div>
+        )
+    }
+}
