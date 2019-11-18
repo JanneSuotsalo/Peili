9ecd6492bbc705746a5aedecca106765206b5a9f
Emil Toivainen <emilanton.toivainen@gmail.com>
2019-11-18 13:20:37 +0200

    Adde gauge meters

--- src/components/card/GasMeter.js
+++ src/components/card/GasMeter.js
@@ -1,14 +1,47 @@
 import React from 'react';
 
+import './GasMeter.css';
+
 export default class GasMeter extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
     render() {
         //now only one item but will be changed to a list
         return (
-            <div className="test">
-            <p>test</p>
+            <div className="GasCard">
+                <div class="progress-circle p50 gauge0">
+                    <span>mittari 1</span>
+                    <div class="left-half-clipper">
+                        <div class="first50-bar"></div>
+                        <div class="value-bar"></div>
+                    </div>
+                </div>
+                <div class="progress-circle p10 gauge1">
+                    <span>mittari 2</span>
+                    <div class="left-half-clipper">
+                        <div class="first50-bar"></div>
+                        <div class="value-bar"></div>
+                    </div>
+
+                </div>
+                <div class="progress-circle p30 gauge2">
+                    <span>mittari 3</span>
+                    <div class="left-half-clipper">
+                        <div class="first50-bar"></div>
+                        <div class="value-bar"></div>
+                    </div>
+
+                </div>
+                <div class="progress-circle p15 gauge3">
+                    <span>mittari 4</span>
+                    <div class="left-half-clipper">
+                        <div class="first50-bar"></div>
+                        <div class="value-bar"></div>
+                    </div>
+
+                </div>
+
             </div>
         );
     }
--- src/components/card/TestCard.js
+++ src/components/card/TestCard.js
@@ -3,29 +3,33 @@ import { Link } from "react-router-dom";
 
 
 export default class TestCard extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
-    style(){
-        return '#'+Math.random().toString(16).substr(-6); 
+    style() {
+        return '#' + Math.random().toString(16).substr(-6);
     }
     handleClick = () => {
         this.props.clicked(this.props.title);
     }
     render() {
         let width1 = "";
-        if(this.props.title % 3 == 0){
+        if (this.props.title % 3 == 0) {
             width1 = "90%"
         } else {
             width1 = "45%"
         }
         return (
-            <div className="TestCard" style={{backgroundImage: 'linear-gradient(' +this.style() +',' +this.style()+')',width:width1}} onClick={this.handleClick}>
+            <div className="TestCard" style={{ backgroundImage: 'linear-gradient(' + this.style() + ',' + this.style() + ')', width: width1 }} onClick={this.handleClick}>
                 <div className="TestCircle">
-                <div className="TestCircle1"></div>                            
-                    </div>                            
-                <h2>Testi {this.props.title}</h2> 
-                <div className="TestWindow"></div>            
+                    <div className="TestCircle1"></div>
+                </div>
+                <div className="TestInfo">
+                    <h2>Testi {this.props.title}</h2>
+                    <p>palkinto: {this.props.title * 100}</p>
+                </div>
+
+                <div className="TestWindow"></div>
             </div>
         );
     }
