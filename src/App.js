ee7dd743fb0cc52d1915304b1cf9c6bd7e010bd0
Nguyen Duc Manh <manhnguyen@Nguyens-MacBook-Pro.local>
2019-11-25 01:03:26 +0200

    Manh's commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,25 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Organization from "./pages/organizations/Organization";
-import Profile from "./pages/profile/Profile";
-import Login from "./pages/signIn/Login"
-import Register from "./pages/signIn/Register"
-import { BrowserRouter, Switch, Route, Link, Router } from "react-router-dom";
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <ul id="nav">
-            <li>
-              <Link to="/profile">
-                <a>Profile</a>
-              </Link>
-            </li>
-            <li>
-              <Link to="/organization">
-                <a>Organization</a>
-              </Link>
-            </li>
-            <li>
-              <Link to="/login">
-                <a>Login</a>
-              </Link>
-            </li>
-            <li>
-              <Link to="/register">
-                <a>Register</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <Route exact path="/">
-          <Profile />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/organization">
-          <Organization />
-        </Route>
-        <Route path="/profile">
-          <Profile />
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+            <div className="App">
+            
+              <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/cart" component={Cart}/>
+                  </Switch>
+             </div>
+       </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
