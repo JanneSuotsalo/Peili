323163b29f0e456cc072137e7e22c865466e999b
Janne Suotsalo <janne.suotsalo@gmail.com>
2019-11-26 22:58:55 +0200

    functional show more button for history

--- src/pages/history/History.js
+++ src/pages/history/History.js
@@ -1,55 +1,85 @@
 import React, { useState, useEffect } from "react";
 import "./History.css"
-//import history from "./History.json"
-import sideImg from "./historySide.png"
 import sideImgEnd from "./historySideEnd.png"
-import sideImgEnd2 from "./Ellipse.png"
+import sideImgStart from "./Ellipse.png"
+import sideImg from "./historySideImg.png"
+
+
 
 export default function History() {
-    const [state, setState] = useState(0);
-    //const [lastDate, setLastDate] = useState("");
-    let buffer = []
-    let number = 0
-    let lastDate = ""
-    let historyData = require("./History.json")
-    // Sorting the json file to be ordered by date
-    historyData.sort((a, b) => new Date(b.date) - new Date(a.date));
+    const [count, setCount] = useState(1);
+    const [historyData, setHistoryData] = useState([]);
+    const [lastIndex, setLastIndex] = useState([-1])
+    const [visible, setVisible] = useState(true)
 
-    getData(3, "2090-04-23T18:25:43.511Z")
+    let historyDatajson = require("./History.json")
+    let lastIndexNumber = lastIndex.pop();
+    let historyDataArray  = [];
 
-    function getData(amount, whereToStartDate){
-        console.log("state:"+number)
-            for (var i = 0; i < historyData.length && number < amount; i++)
-                {
-                var obj = historyData[i];
-                let container = []
+    function changeLastIndex(index){
+        lastIndex.push(index)
 
-                if(new Date(obj.date) < new Date(whereToStartDate)){
-                    console.log("smaller")
-                    buffer.push(<div className={'history_' + obj.tag.toString() +"Container"}>{container}</div>);
-
-                    // uses the right image on the left
-                    if(i+1 === amount){
-                    container.push(<img className="history_sideImgEnd" src={sideImgEnd2}/>);
-                        if(number +1 === amount){
-                            lastDate = obj.date;
-                        }
-                    } else {
-                    container.push(<img className="history_sideImg" src={sideImg}/>);
-                    }
-                    container.push(<div className="history_date">{obj.date}</div>);
-                    container.push(<div className="history_event">{obj.event}</div>);
-                   number += 1
-            }
+        // hides the button if there is no more history to show
+        if(lastIndexNumber+5 === historyDatajson.length){
+            setVisible(false)
         }
     }
 
+    console.log("Lastindex: " + lastIndex + " Visibility: " + visible + " Count: " + count)
+
+    historyData.push(AddElement(lastIndexNumber, visible, changeLastIndex))
+
+    historyData.forEach(el => {
+        historyDataArray.push(el)
+    });
 
     return (
-        <div className="history_container">
-            {buffer}
-            <button onClick={() => getData(5,lastDate)}>Show more</button>
+        <div>
+            {historyDataArray}
+            {visible && <div className="history_buttonContainer"><button className="history_ShowMoreButton" onClick={() =>setCount(count + 1)}>Show more</button></div>}
+            <div className="history_emptySpace"></div>
         </div>
+    );  
+}
+
+const AddElement = (lastIndex, visible, changevalue) => {
+
+    let historyData = require("./History.json")
+    let timesToRun = 0  
+    let data = []
+
+    // Sorting the json file to be ordered by date
+    historyData.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    for (var i = 0; i < historyData.length && timesToRun < 4 ; i++)
+        {
+            var obj = historyData[i];
+
+            let container = []
+
+            if(i > lastIndex){
+
+                data.push(<div className={'history_' + obj.tag.toString() +"Container"}>{container}</div>);
+
+                if(i === 0){
+                    container.push(<img className="history_sideImgStart" src={sideImgStart}/>);
+                    container.push(<div className="history_dateStart">{obj.date}</div>);
+                    container.push(<div className="history_eventStart">{obj.event}</div>);
+                } else {
+                    container.push(<img className="history_sideImg" src={sideImg}/>);
+                    container.push(<div className="history_date">{obj.date}</div>);
+                    container.push(<div className="history_event">{obj.event}</div>);
+                }
+
+                lastIndex = i;
+                changevalue(lastIndex)
+
+                timesToRun += 1;
+        }
+    }
+
+    return(
+        <div className="history_container">{data}</div>
     );
 }
 
