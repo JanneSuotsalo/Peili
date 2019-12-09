82004213d9a95c44b828f0079309ef4cb17d8842
Janne Suotsalo <janne.suotsalo@gmail.com>
2019-12-09 23:33:25 +0200

    commented history page

--- src/pages/history/History.js
+++ src/pages/history/History.js
@@ -15,17 +15,20 @@ export default function History() {
   let historyDataArray = [];
   let runOnceCheck = runOnce.pop();
 
+  // Handles showing history when you enter the page, runs only one time.
   if (runOnceCheck <= 0) {
     runOnce.push(1);
     historyData.push(AddElement(-1, changeLastIndex));
   }
 
+  // Handles rendering the history data. Also removes the old last index number.
   function render() {
     lastIndexNumber = indexArray.pop();
     setCount(count + 1);
     historyData.push(AddElement(lastIndexNumber, changeLastIndex));
   }
 
+  // Changes the index number and handles hiding "show more" button
   function changeLastIndex(index) {
     indexArray.push(index);
 
@@ -35,6 +38,7 @@ export default function History() {
     }
   }
 
+  // For each element in history data, pushes them into array which shows data on the page
   historyData.forEach(el => {
     historyDataArray.push(el);
   });
@@ -54,6 +58,7 @@ export default function History() {
   );
 }
 
+// Handles creating history
 const AddElement = (lastIndex, changevalue) => {
   let historyData = require("./History.json");
   let timesToRun = 0;
@@ -62,16 +67,20 @@ const AddElement = (lastIndex, changevalue) => {
   // Sorting the json file to be ordered by date
   historyData.sort((a, b) => new Date(b.date) - new Date(a.date));
 
+  // Goes through the data json and creates the history. 
   for (var i = 0; i < historyData.length && timesToRun < 6; i++) {
     var obj = historyData[i];
     let container = [];
 
+    // First checks that the new history data's index that is wanted to be rendered, is higher than the last index. This way we get the older data from the json that has not been shown already.
     if (i > lastIndex) {
       data.push(
         <div className={"history_" + obj.tag.toString() + "Container"}>
           {container}
         </div>
       );
+
+      // Gives different image for the first history data
       if (i === 0) {
         container.push(
           <img className="history_sideImgStart" src={sideImgStart} />
@@ -84,12 +93,15 @@ const AddElement = (lastIndex, changevalue) => {
         container.push(<div className="history_event">{obj.event}</div>);
       }
 
+      // Changes value of last index, so the program will know where to continue the history from
       lastIndex = i;
       changevalue(lastIndex);
 
+      // Increases the number till the loop has run 6 times
       timesToRun += 1;
     }
   }
 
+  // Returns a div which contains the history data
   return <div key={lastIndex} className="history_container">{data}</div>;
 };
