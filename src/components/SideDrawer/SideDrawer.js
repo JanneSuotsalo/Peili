168182f1e9398f839ad8fa9be5a3e519a80f5283
timpec <timpe96@hotmail.com>
2019-12-10 14:44:55 +0200

    sidedrawer fix

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -16,37 +16,37 @@ const sideDrawer = props => {
     <nav className={drawAnim}>
       <ul>
         <Link to="/profile">
-          <li className="first">
-            <a onClick={props.closeDraw}>{I18n.t("navBar.profile")}</a>
+          <li onClick={props.closeDraw} className="first">
+            <a>{I18n.t("navBar.profile")}</a>
           </li>
         </Link>
         <Link to="/Feed">
-          <li>
-            <a onClick={props.closeDraw}>{I18n.t("navBar.mainFeed")}</a>
+          <li onClick={props.closeDraw}>
+            <a>{I18n.t("navBar.mainFeed")}</a>
           </li>
         </Link>
         <Link to="/organization">
-          <li>
-            <a onClick={props.closeDraw}>{I18n.t("navBar.organisations")}</a>
+          <li onClick={props.closeDraw}>
+            <a>{I18n.t("navBar.organisations")}</a>
           </li>
         </Link>
         <Link to="/test">
-          <li>
-            <a onClick={props.closeDraw}>{I18n.t("navBar.test")}</a>
+          <li onClick={props.closeDraw}>
+            <a>{I18n.t("navBar.test")}</a>
           </li>
         </Link>
         <Link to="/history">
-          <li>
-            <a onClick={props.closeDraw}>{I18n.t("navBar.history")}</a>
+          <li onClick={props.closeDraw}>
+            <a>{I18n.t("navBar.history")}</a>
           </li>
         </Link>
         <Link to="/shop">
-          <li>
-            <a onClick={props.closeDraw}>{I18n.t("Kauppa")}</a>
+          <li onClick={props.closeDraw}>
+            <a>{I18n.t("Kauppa")}</a>
           </li>
         </Link>
-        <li>
-          <a onClick={props.closeDraw}>{I18n.t("Tavaraluettelo")}</a>
+        <li onClick={props.closeDraw}>
+          <a>{I18n.t("Tavaraluettelo")}</a>
         </li>
         <div className="profileCorner">
           <img src={exampleIcon}></img>
